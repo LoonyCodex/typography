e954a78f5491e739b7cdac3b3984b80ce511f6a7
Poly Devil <polydevil@cocaine.ninja>
2022-01-19 10:11:50 +0300

    clean up

---
+++ scripts/build.cjs
@@ -0,0 +1,30 @@
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
+const { vanillaExtractPlugin } = require("@vanilla-extract/esbuild-plugin");
+
+async function processCss(css) {
+  const result = await postcss([autoprefixer]).process(css, {
+    from: undefined /* suppress source map warning */,
+  });
+
+  return result.css;
+}
+
+require("esbuild")
+  .build({
+    entryPoints: ["./src/index.gen.ts"],
+    bundle: true,
+    sourcemap: true,
+    // minify: true,
+    splitting: true,
+    outdir: 'dist',
+    plugins: [
+      vanillaExtractPlugin({
+        processCss,
+      }),
+    ],
+    format: "esm",
+    target: "es6",
+    external: ["react", "react-dom"],
+  })
+  .catch(() => process.exit(1));
