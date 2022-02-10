575be1c7246be87b282ed2e662ccf9fa1b2d32ab
Poly Devil <polydevil@cocaine.ninja>
2022-02-10 07:01:48 +0300

    add storybook

---
+++ .storybook/main.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.js"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-postcss",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react"
+}
