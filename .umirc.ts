ce2942fbdecbfbb10bb0778205f54af82e3ec25e
Poly Devil <polydevil@cocaine.ninja>
2022-02-15 21:32:47 +0300

    update umi

--- .umirc.ts
+++ .umirc.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  outputPath: 'docs-dist',
+  outputPath: './.umi/package',
 
   // styles: [path.resolve(__dirname, './semicolon/index.gen.css')],
 
@@ -21,6 +21,6 @@ export default defineConfig({
   // ],
 
   alias: {
-    'typography': path.resolve(__dirname, './semicolon/index.gen.js'),
+    'typography': path.resolve(__dirname, './package/index.gen.js'),
   },
 });
