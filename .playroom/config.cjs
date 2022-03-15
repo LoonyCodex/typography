2f18e545b34efae5f3ba662e42c8880f51c9ea04
Poly Devil <polydevil@cocaine.ninja>
2022-03-15 10:11:40 +0300

    Update config.cjs

--- .playroom/config.cjs
+++ .playroom/config.cjs
@@ -9,16 +9,16 @@ module.exports = {
   openBrowser: true,
   exampleCode:
 `
-<H1>Welcome</H1>
-<H2>This is interactive playground</H2>
-<H3>You can press on <Keyboard>+</Keyboard> button to open a snippets panel, so you can explore components</H3>
+<H1 marginBlockEnd="size200">Welcome</H1>
+<H2 marginBlockEnd="size400">This is interactive playground</H2>
+<H3 marginBlockEnd="size600">You can press on <Keyboard>+</Keyboard> button to open a snippets panel, so you can explore components</H3>
 
 <Paragraph>
   Source code can be found on <Link href="https://github.com/LoonyCodex/typography">LoonyCodex/typography</Link>
   <LineBreak />
   You can also refer to a <Link href="https://loonycodex.github.io/typography/">docs</Link>
   <LineBreak />
-  <Disclaimer>Build with <Important><Link href="https://rescript-lang.org/" color="blue700">Rescript</Link></Important> and <Important><Link href="https://vanilla-extract.style/" color="blue700">vanilla-extract</Link></Important></Disclaimer>
+  <Disclaimer>Build with <Link href="https://rescript-lang.org/" color="blue700" fontWeight="heavy">Rescript</Link> and <Link href="https://vanilla-extract.style/" color="blue700" fontWeight="heavy">vanilla-extract</Link></Disclaimer>
 </Paragraph>
 `,
   webpackConfig: () => ({
